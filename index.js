31be14a43fcd1affbc44a802b652145e852f5927
mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2022-12-26 20:55:19 +0200

    step 3

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ app.get("/", (req, res)=>{
     res.send("ok")
 })
 
+app.get("/test", (req, res) => {
+    res.json({ status: 200, message: 'ok' });
+  });
+
+app.get("/time", (req, res)=>{
+    const curretnTime = new Date();
+    const hours = curretnTime.getHours();
+    const minutes = curretnTime.getMinutes();
+    res.json({status: 200, message: `${hours}:${minutes}` })
+})
+
 app.listen(3000, ()=>{
     console.log("Server started on port 3000");
 })
