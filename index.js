71067d9f9ad43f07fbd83f2da81e59c88f9072d8
mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2022-12-27 13:27:36 +0200

    step 5

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب', year: 1992, rating: 6.2 }
+]
+
 app.get("/", (req, res) => {
     res.send("ok")
 })
@@ -30,6 +37,23 @@ app.get("/search", (req, res) => {
         res.status(500).json({ status: 500, error: true, message: "you have to provide a search" })
     }
 })
+
+app.get("/movies/add", (req,res)=>{
+    
+})
+
+app.get("/movies/get", (req, res) => {
+    res.json({ status: 200, data: movies })
+})
+
+app.get("/movies/add", (req,res)=>{
+
+})
+
+app.get("/movies/add", (req,res)=>{
+
+})
+
 
 
 app.listen(3000, () => {
