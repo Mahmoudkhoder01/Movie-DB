f124babeb93f2fdb751a01b5b69b313467377b7e
mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2022-12-27 13:15:33 +0200

    step 4

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.send("ok")
 })
 
 app.get("/test", (req, res) => {
     res.json({ status: 200, message: 'ok' });
-  });
+});
 
-app.get("/time", (req, res)=>{
+app.get("/time", (req, res) => {
     const curretnTime = new Date();
     const hours = curretnTime.getHours();
     const minutes = curretnTime.getMinutes();
-    res.json({status: 200, message: `${hours}:${minutes}` })
+    res.json({ status: 200, message: `${hours}:${minutes}` })
 })
 
-app.listen(3000, ()=>{
+
+app.get('/hello/:id', (req, res) => {
+    const id = req.params.id;
+    res.status(200).json({ status: 200, message: `Hello, ${id}` });
+});
+
+app.get("/search", (req, res) => {
+    const search = req.query.search
+    if (search) {
+        res.status(200).json({ status: 200, message: "ok", data: search })
+    } else {
+        res.status(500).json({ status: 500, error: true, message: "you have to provide a search" })
+    }
+})
+
+
+app.listen(3000, () => {
     console.log("Server started on port 3000");
 })
